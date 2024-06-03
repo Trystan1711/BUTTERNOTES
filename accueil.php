f708a607488f8d0b114ccc874efb31d6145e63bd
AxelKuzan <168738971+AxelKuzan@users.noreply.github.com>
2024-06-03 16:31:18 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../butter/style1.css" rel="stylesheet" type="text/css">
+    <title>ButterNotes - Votre répértoires de notes</title>
+</head>
+
+<header>
+    <nav>
+        <a href="../butter/accueil.html">Accueil</a>
+        <a href="../butter/noteseleves.html">Notes</a>
+        <a href="../butter/compte.html">Profil</a>
+    </nav>
+</header>
+<body>
+    <center><form action="form.php" method="POST">
+        <label for="login">Login:</label><br>
+            <input type="text" id="login" name="login"><br>
+            <label for="password">Mot de passe:</label><br>
+            <input type="password" id="password" name="password"><br><br> 
+            <input type="submit" value="Valider">
+        </center>
+        
+    
+</body>
+</html>
