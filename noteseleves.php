434682a4176172117dd61db06340c3a30b3340d0
Trystan1711 <165803567+Trystan1711@users.noreply.github.com>
2024-06-04 12:31:26 +0200

    Update noteseleves.php

--- noteseleves.php
+++ noteseleves.php
@@ -3,8 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/compte.css" rel="stylesheet">
     <title>Document</title>
 </head>
+<header>
+    <nav>
+        <a href="profil_eleve.php">Profil</a>
+        <a href="noteseleves.php">Notes</a>
+        
+    </nav>
+</header>
 <body>
     
 </body>
