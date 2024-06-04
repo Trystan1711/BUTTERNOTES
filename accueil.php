591449f84c3b4e7842c089fb16e57bb2eac7b00d
Trystan1711 <165803567+Trystan1711@users.noreply.github.com>
2024-06-04 10:05:18 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -3,16 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../butter/style1.css" rel="stylesheet" type="text/css">
+    <link href="css/connexion.css" rel="stylesheet" type="text/css">
     <title>ButterNotes - Votre répértoires de notes</title>
 </head>
 
-<header>
-    <nav>
-        <a href="../butter/accueil.html">Accueil</a>
-        <a href="../butter/noteseleves.html">Notes</a>
-        <a href="../butter/compte.html">Profil</a>
-    </nav>
+
 </header>
 <body>
     <center><form action="form.php" method="POST">
@@ -20,9 +15,7 @@
             <input type="text" id="login" name="login"><br>
             <label for="password">Mot de passe:</label><br>
             <input type="password" id="password" name="password"><br><br> 
-            <input type="submit" value="Valider">
-        </center>
-        
-    
+            <button class="button" ><span><a class="bouton_connexion" href="compte.php">Connexion</a></span></button>
+        </center> 
 </body>
 </html>
