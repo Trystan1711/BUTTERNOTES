19da8370456a18bdc53e135ebaab677231ad6b49
DarelYKC <darelyokingchuen@gmail.com>
2024-06-15 23:27:34 +0200

    Add files via upload

---
+++ matieres_admin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/admin.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<header>
+    <nav>
+        <a href="profil_admin.php">Profil</a>
+        <a href="matieres_admin.php">Matière</a>
+        <a href="prof.php">Enseignant</a>
+        <a href="etudiant.php">Etudiant</a>
+    </nav>
+
+
+</header>
+  <div classe="non">
+<table>
+  <caption>
+  </caption>
+  <thead>
+    <tr>
+      <th scope="col">Person</th>
+      <th scope="col">Most interest in</th>
+      <th scope="col">Age</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Chris</th>
+      <td>HTML tables</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <th scope="row">Dennis</th>
+      <td>Web accessibility</td>
+      <td>45</td>
+    </tr>
+    <tr>
+      <th scope="row">Sarah</th>
+      <td>JavaScript frameworks</td>
+      <td>29</td>
+    </tr>
+    <tr>
+      <th scope="row">Karen</th>
+      <td>Web performance</td>
+      <td>36</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <th scope="row" colspan="2">Average age</th>
+      <td>33</td>
+    </tr>
+  </tfoot>
+</table>
+</div> 
